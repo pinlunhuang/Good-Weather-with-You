3deaf296e93d08ccccb98e5a958c392c5fa65622
Pinlun <lunwaac@gmail.com>
2020-03-25 14:14:13 +0800

    Added URLSession & Task to get weather data response

--- Good Weather with You/Model/WeatherManager.swift
+++ Good Weather with You/Model/WeatherManager.swift
@@ -9,10 +9,32 @@
 import Foundation
 
 struct WeatherManager {
-    let apiURL = "http://api.openweathermap.org/data/2.5/weather?appid=f4d58e5ec39cb74e6c0c582fbd7fa2ea&units=metric"
+    let apiURL = "https://api.openweathermap.org/data/2.5/weather?appid=f4d58e5ec39cb74e6c0c582fbd7fa2ea&units=metric"
     
     func getWeather(city: String) {
-        let urlString = "\(apiURL)&q=\(city)"
-        print(urlString)
+        let url = "\(apiURL)&q=\(city)"
+        sendRequest(urlString: url)
+    }
+    
+    func sendRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url, completionHandler: processResponse(data:response:error:))
+            
+            task.resume()
+        }
+    }
+    
+    func processResponse(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let responseData = data {
+            let dataString = String(data: responseData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
