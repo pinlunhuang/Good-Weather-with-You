5799811677f5a2fd866847767d494c4ff4b46e23
Pinlun <lunwaac@gmail.com>
2020-03-25 14:31:10 +0800

    Added JSONDecoder to decode response data

---
+++ Good Weather with You/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Good Weather with You
+//
+//  Created by Pinlun on 2020/3/25.
+//  Copyright © 2020 Pinlun. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
--- Good Weather with You/Model/WeatherManager.swift
+++ Good Weather with You/Model/WeatherManager.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 struct WeatherManager {
+    
     let apiURL = "https://api.openweathermap.org/data/2.5/weather?appid=f4d58e5ec39cb74e6c0c582fbd7fa2ea&units=metric"
     
     func getWeather(city: String) {
@@ -20,21 +21,28 @@ struct WeatherManager {
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
             
-            let task = session.dataTask(with: url, completionHandler: processResponse(data:response:error:))
-            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let responseData = data {
+                    self.parseJsonData(weatherData: responseData)
+                }
+            }
             task.resume()
         }
     }
     
-    func processResponse(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
+    func parseJsonData(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
         
-        if let responseData = data {
-            let dataString = String(data: responseData, encoding: .utf8)
-            print(dataString)
-        }
     }
 }
